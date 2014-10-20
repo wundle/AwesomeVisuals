9f616e3d1b1dd1537c6383d40e6cf8bd284332d7
Monkey13oy <garethgoodram@gmail.com>
2014-10-20 14:05:40 +0100

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,14 @@
++function () {
+
+	$("#male").AwesomeVisuals();
+
+	$("#heart").AwesomeVisuals({
+		fontawesomeIcon: "heart",
+		fillProportion: 0.75,
+		fillAdjustment:{
+			top: 25,
+			bottom: 25
+		}
+	});
+
+}();
